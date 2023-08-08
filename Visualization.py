35d84568ba2c0cfb2c44c1464adca9d347fceaa9
Mahdishehady <shehadi2000@gmail.com>
2023-08-08 11:53:41 +0300

    create master

---
+++ Visualization.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import noSQL
+
+date1 = datetime(2023, 7, 20)
+date2 = datetime(2023, 7, 27)
+print(str(date1.date()))
+
+dic_news = noSQL.news_by_dates(date1, date2)
+
+# Extract keys and values from the dictionary
+categories = list(dic_news.keys())
+values = list(dic_news.values())
+
+# Create a bar chart
+plt.bar(categories, values)
+
+# Add labels and title
+plt.xlabel('Categories')
+plt.ylabel('Values')
+plt.title('Dictionary Visualization')
+
+# Rotate x-axis labels for better readability
+# plt.xticks(rotation=10)
+
+# Display the chart
+plt.show()
--- noSQL.py
+++ noSQL.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pymongo import MongoClient
 
 
@@ -10,7 +12,7 @@ def count_eachDoc():
     pipeline = [
         {
             "$group": {
-                "_id": "$_id",
+                "_id": "$publishedDate",
                 "count": {"$sum": 1}
             }
         }
@@ -18,14 +20,79 @@ def count_eachDoc():
 
     # Execute the aggregation query
     result = collection.aggregate(pipeline)
-    get_dup = []
+    # get_dup = []
     # Print the aggregation result
     for entry in result:
-        if entry['count'] == 2:
-            get_dup.append(entry)
-    print(get_dup)
+        # if entry['count'] == 2:
+        # get_dup.append(entry)
+        print(entry)
     # Close the connection
     client.close()
 
 
-count_eachDoc()
+# count_eachDoc()
+
+
+def count_documents_by_published_date(date):
+    client = MongoClient('mongodb://localhost:27017')
+    db = client['news_db']
+    collection = db['my_database']
+
+    # Aggregation pipeline
+    pipeline = [
+        {
+            "$match": {
+                "publishedDate": {"$regex": date}
+            }
+        },
+        {
+            "$group": {
+                "_id": "$publishedDate",
+                "count": {"$sum": 1}
+            }
+        }
+    ]
+
+    # Execute the aggregation query
+    result = collection.aggregate(pipeline)
+
+    # Print the aggregation result
+    list_news = []
+    doc_add = 0
+    for entry in result:
+        list_news.append(entry)
+    for x in range(0, len(list_news)):
+        doc_add += int(list_news[x]['count'])
+    # Close the connection
+    client.close()
+    print(doc_add)
+    return doc_add
+
+
+published_date = "2023-07-20"
+
+
+# count_documents_by_published_date(published_date)
+
+
+def get_keywords():
+    client = MongoClient('mongodb://localhost:27017')
+    db = client['news_db']
+    collection = db['my_database']
+
+    # Query the collection and retrieve the "keywords" field
+    keywords_list = collection.distinct('keywords')
+
+    return keywords_list
+
+
+get_keywords()
+
+
+def news_by_dates(get_start_date, get_end_date):
+    data = {}
+    while not str(get_start_date.date()) == str(get_end_date.date()):
+        data[str(get_start_date.date())] = count_documents_by_published_date(str(get_start_date.date()))
+        get_start_date = get_start_date + datetime.timedelta(days=1)
+
+    return data
